0176cada50447f3fcdf7415e9fe9224e9770f20c
HaniLeb <hani.lebguiret@epsi.fr>
2023-06-07 16:23:24 +0200

    Test de 9 à 20

--- RomanNumbers.Test/RomanNumberTest.cs
+++ RomanNumbers.Test/RomanNumberTest.cs
@@ -1,10 +1,19 @@
+using Xunit.Abstractions;
+
 namespace RomanNumbers.Test;
 
 public class RomanNumberTest
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public RomanNumberTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Theory(DisplayName = "ETANT DONNE le chiffre <nombreUnités> " +
-                        "QUAND je le convertis en nombres romains " +
-                        "ALORS j'obtiens <nombreUnités> fois I")]
+                          "QUAND je le convertis en nombres romains " +
+                          "ALORS j'obtiens <nombreUnités> fois I")]
     [InlineData(1)]
     [InlineData(2)]
     [InlineData(3)]
@@ -41,5 +50,85 @@ public class RomanNumberTest
         
         var suiteDeI = new string('I', (int) nombreUnités - 5);
         Assert.Equal("V" + suiteDeI, nombreRomain);
+    }
+
+    [Fact(DisplayName = "ETANT DONNE le chiffre 9 " +
+                        "QUAND je le convertis en nombres romains " +
+                        "ALORS j'obtiens IX")]
+    public void TestNeuf()
+    {
+        const uint chiffreArabe = 9;
+
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(chiffreArabe);
+
+        Assert.Equal("IX", nombreRomain);
+    }
+
+    [Theory(DisplayName = "ETANT DONNE un nombre <nombreUnités> compris entre 10 et 13 " +
+                          "QUAND je le convertis en nombres romains " +
+                          "ALORS j'obtiens V plus <(nombreUnités-10)> fois I")]
+    [InlineData(10)]
+    [InlineData(11)]
+    [InlineData(12)]
+    [InlineData(13)]
+    public void TestDixPlusUnité(uint nombreUnités)
+    {
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(nombreUnités);
+
+        var suiteDeI = new string('I', (int)nombreUnités - 10);
+        _testOutputHelper.WriteLine(suiteDeI);
+        Assert.Equal("X" + suiteDeI, nombreRomain);
+    }
+
+    [Fact(DisplayName = "ETANT DONNE le chiffre 14 " +
+                        "QUAND je le convertis en nombres romains " +
+                        "ALORS j'obtiens XIV")]
+    public void TestQuatorze()
+    {
+        const uint chiffreArabe = 14;
+
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(chiffreArabe);
+
+        Assert.Equal("XIV", nombreRomain);
+    }
+
+    [Theory(DisplayName = "ETANT DONNE un nombre <nombreUnités> compris entre 15 et 18 " +
+                          "QUAND je le convertis en nombres romains " +
+                          "ALORS j'obtiens V plus <(nombreUnités-5)> fois I")]
+    [InlineData(15)]
+    [InlineData(16)]
+    [InlineData(17)]
+    [InlineData(18)]
+    public void TestQuinzePlusUnité(uint nombreUnités)
+    {
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(nombreUnités);
+
+        var suiteDeI = new string('I', (int)nombreUnités - 15);
+        _testOutputHelper.WriteLine(suiteDeI);
+        Assert.Equal("XV" + suiteDeI, nombreRomain);
+    }
+
+    [Fact(DisplayName = "ETANT DONNE le chiffre 19 " +
+                        "QUAND je le convertis en nombres romains " +
+                        "ALORS j'obtiens XIX")]
+    public void TestDixNeuf()
+    {
+        const uint chiffreArabe = 19;
+
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(chiffreArabe);
+
+        Assert.Equal("XIX", nombreRomain);
+    }
+
+    [Fact(DisplayName = "ETANT DONNE le chiffre 20 " +
+                        "QUAND je le convertis en nombres romains " +
+                        "ALORS j'obtiens XX")]
+    public void TestVingt()
+    {
+        const uint chiffreArabe = 20;
+
+        var nombreRomain = ConvertisseurNombresRomains.Convertir(chiffreArabe);
+
+        Assert.Equal("XX", nombreRomain);
     }
 }
--- RomanNumbers/ConvertisseurNombresRomains.cs
+++ RomanNumbers/ConvertisseurNombresRomains.cs
@@ -3,10 +3,38 @@
 public static class ConvertisseurNombresRomains
 {
     public static string Convertir(uint chiffreArabe)
-        => chiffreArabe switch
-           {
-               <= 3 => new string('I', (int)chiffreArabe),
-               4    => "IV",
-               _    => "V" + Convertir(chiffreArabe - 5)
-           };
+    {
+        switch (chiffreArabe)
+        {
+            case <= 3:
+                return new string('I', (int)chiffreArabe);
+            case 4:
+                return "IV";
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return "V" + Convertir(chiffreArabe - 5);
+            case 9:
+                return "IX";
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+                return "X" + Convertir(chiffreArabe - 10);
+            case 14:
+                return "XIV";
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                return "XV" + Convertir(chiffreArabe - 15);
+            case 19:
+                return "XIX";
+            case 20:
+                return "XX";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(chiffreArabe));
+        }
+    }
 }
